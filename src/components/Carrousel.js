018258e12fa1a6cba70b6ad9f8ae1e286330b4e2
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2023-05-26 08:05:57 +0200

    change 'collapseTitle' to 'title'

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -8,10 +8,13 @@ const Carrousel = (props) => {
     let [currentSlide, setCurrentSlide] = useState(0);
     return <div className="carrousel">
         { props.pictures.length > 1 &&
-            <span className="arrows"  >
+        <>
+            <span className="arrows">
                 <img src={arrowLeft} className="carrouselArrowLeft" onClick={()=>setCurrentSlide(currentSlide === 0 ? props.pictures.length-1 : (currentSlide-1) % props.pictures.length)}/>
                 <img src={arrowRight} className="carrouselArrowRight" onClick={()=>setCurrentSlide((currentSlide+1) % props.pictures.length )}/>
             </span>
+            <span className="currentSlide">{currentSlide+1} / {props.pictures.length}</span>
+        </>
         }
         <img src={props.pictures[currentSlide]} className="carrouselPicture"/>
     </div>
