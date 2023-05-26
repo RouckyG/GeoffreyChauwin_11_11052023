845cd1c3ca78e464c791b1cb7495df27c750a6a2
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2023-05-26 14:03:35 +0200

    add rating component + add responsive for tablet and mobile

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate  } from 'react-router-dom';
 import Carrousel from "../components/Carrousel";
 import Tags from "../components/Tags";
+import Rating from "../components/Rating";
 import Collapse from "../components/Collapse";
 
 const Location = (props) => {
-
     const params = useParams();
     const [locations, setLocations] = useState();
     const [location, setLocation] = useState();
@@ -18,47 +18,57 @@ const Location = (props) => {
         setLocations(response);
     };
 
+    const navigate = useNavigate();
+
     useEffect(() => {
         
         if (locations) {
-            setLocation(locations.find(
+            let location = locations.find(
                 (location) => location.id === params.id
-            ));
+            );
+            if(location === undefined){
+                navigate("/*")
+            }
+            else{
+                setLocation(location)
+            }
         }
         else{
             getLocations();
         }
+
     }, [locations]);
 
     return <div className="locationPage">
         {location && 
         <>
             <Carrousel pictures={location.pictures} />
-            <>
-                <>
+            <article className="locationDetails">
+                <div className="locationTitle">
                     <h2>
                         {location.title}
                     </h2>
                     <h3>
                         {location.location}
                     </h3>
-                    <Tags tags={location.tags} />
-                </>
-                <>
+                </div>
+                <div className="locationHost">
                     <h3>
-                        {location.host.name}
+                        {location.host.name.split(" ")[0]}
+                        <br/>
+                        {location.host.name.split(" ")[1]}
                     </h3>
                     <img src={location.host.picture} alt={"photo de profil de " + location.host.name}/>
-                </>
-            </>
-            <>
+                </div>
+                <Tags tags={location.tags} />
+                <Rating rating={location.rating} />
                 <Collapse title="Description">
-                    <p>{location.description}</p>
+                <p>{location.description}</p>
                 </Collapse>
                 <Collapse title="Équipements">
                     {location.equipments.map((equipment)=> {return <p>{equipment}</p>})}
                 </Collapse>
-            </>
+            </article>
         </>
         }
     </div>
